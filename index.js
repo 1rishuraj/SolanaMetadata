f749bec37bebf93e978977fc19b3bfcb4d9ddaf6
Rishu Raj <rishuraj1702@gmail.com>
2025-07-07 22:33:52 +0530

    finale

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json())
 // CORS Configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // Allow only this origin
+  origin: 'https://token-launcher-nine.vercel.app/', // Allow only this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
   credentials: true, // Allow credentials (if needed)
