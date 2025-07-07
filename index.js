286e90d8b4fe8957e81367b3061048e0602e4424
Rishu Raj <rishuraj1702@gmail.com>
2025-07-07 22:49:49 +0530

    yetoh shuruat hai

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
 import express from "express";
-import User from './db.js'
-import cors from 'cors'
+import User from './db.js';
+import cors from 'cors';
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 const allowedOrigins = [
-    'http://localhost:5173', 
-    'https://token-launcher-nine.vercel.app',
-    'https://token-launcher-rishu-rajs-projects-d80232cf.vercel.app',
-    'https://token-launcher-git-main-rishu-rajs-projects-d80232cf.vercel.app/'
-    // Add more allowed origins as necessary
+  'http://localhost:5173',
+  'https://token-launcher-nine.vercel.app',
+  'https://token-launcher-rishu-rajs-projects-d80232cf.vercel.app',
+  'https://token-launcher-git-main-rishu-rajs-projects-d80232cf.vercel.app'
 ];
-// Middleware
+
+// Handle preflight OPTIONS requests globally
+app.options('*', cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    const normalizedOrigin = origin.replace(/\/$/, '');
+    if (allowedOrigins.includes(normalizedOrigin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+// Use CORS middleware for all other requests
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps, curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } else {
-            return callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // Allow credentials
-}));// allows only our frontend client localhost to access data from server running '/' etc endpoints. & blocks other website to access our endpoints data
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    const normalizedOrigin = origin.replace(/\/$/, '');
+    if (allowedOrigins.includes(normalizedOrigin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
+// Your routes
+app.post('/api', async (req, res) => {
+  const { name, symbol, image } = req.body;
+  const description = "Token Description";
+  const newUser = await User.create({ name, symbol, description, image });
+  res.json({ msg: "created", tokenID: newUser._id });
+});
 
-app.post('/api',async function(req,res){
-  const name=req.body.name
-  const symbol=req.body.symbol
-  const description="Token Description"
-  const image=req.body.image;
-
-  const newUser=await User.create({
-    name,symbol,description,image
-  })
-
-  res.json({
-    msg:"created",
-    tokenID:newUser._id
-  })
-})
-app.get("/metadata/:tokenID",async function (req, res) {
-  const TokenID=req.params.tokenID
-  const user=await User.findOne({
-    _id:TokenID
-  })
+app.get("/metadata/:tokenID", async (req, res) => {
+  const TokenID = req.params.tokenID;
+  const user = await User.findOne({ _id: TokenID });
   res.json({
     name: user.name,
     symbol: user.symbol,
@@ -55,4 +62,5 @@ app.get("/metadata/:tokenID",async function (req, res) {
     image: user.image,
   });
 });
+
 app.listen(3000);
